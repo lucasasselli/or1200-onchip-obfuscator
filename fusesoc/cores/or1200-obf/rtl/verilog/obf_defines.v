c2eb496e292621f73c5800f19deb7e3b35451fa0
Luca Sasselli <4eversax@gmail.com>
2017-10-07 16:37:18 +0200

    Implemented type F and FI instructions

--- fusesoc/cores/or1200-obf/rtl/verilog/obf_defines.v
+++ fusesoc/cores/or1200-obf/rtl/verilog/obf_defines.v
@@ -3,6 +3,9 @@
 `define OBF_CMD_WIDTH 16
 `define OBF_KEY_WIDTH 2
 
+// Obfuscator enable counter
+`define OBF_ENCNT_WIDTH 8
+
 // LUT address fields
 `define OBF_IGU_WIDTH 7
 `define OBF_SUB_WIDTH 2 // TODO Better addressing mode
@@ -18,6 +21,7 @@
 `define OBF_INSN_TYPE_I  3'b010 // Immediate
 `define OBF_INSN_TYPE_M  3'b011 // Memory
 `define OBF_INSN_TYPE_F  3'b100 // Flag
-`define OBF_INSN_TYPE_S  3'b101 // System
-`define OBF_INSN_TYPE_B  3'b110 // Branch
+`define OBF_INSN_TYPE_FI 3'b101 // Flag immediate
+`define OBF_INSN_TYPE_S  3'b110 // System
+`define OBF_INSN_TYPE_B  3'b111 // Branch
 
--- fusesoc/cores/or1200-obf/rtl/verilog/obf_insngen.v
+++ fusesoc/cores/or1200-obf/rtl/verilog/obf_insngen.v
@@ -90,6 +90,18 @@ always @(f_in_opc) begin
         // A-type
         f_in_type = `OBF_INSN_TYPE_A;
     end
+    else if(
+        f_in_opc == `OR1200_OR32_SFXX
+    ) begin
+        // F-type
+        f_in_type = `OBF_INSN_TYPE_F;
+    end
+    else if(
+        f_in_opc == `OR1200_OR32_SFXXI
+    ) begin
+        // FI-type
+        f_in_type = `OBF_INSN_TYPE_FI;
+    end
     else begin
         // TODO Place holder
         f_in_type = `OBF_INSN_TYPE_N;
@@ -102,17 +114,23 @@ wire [11:0]                     sw_cmd  = lut_out_sub[12:1];
 wire                            sw_last = lut_out_sub[0];
 
 // Generate output fields using cmd
-wire [5:0]  f_out_OPC0 = sw_cmd[11:6];
-wire [3:0]  f_out_OPC1 = sw_cmd[11:8];
-wire [3:0]  f_out_OPC2 = sw_cmd[7:4];
-wire [4:0]  f_out_D    = sw_cmd[3] ? 5'b00000 : f_in_D;
+wire [5:0]  f_out_OPC   = sw_cmd[11:6];
+wire [3:0]  f_out_AOPC1 = sw_cmd[11:8];
+wire [3:0]  f_out_AOPC2 = sw_cmd[7:4];
+wire [4:0]  f_out_FOPC  = sw_cmd[11:7];
+wire [4:0]  f_out_D     = sw_cmd[3] ? 5'b00000 : f_in_D;
 
-wire [4:0]  f_out_A    = sw_cmd[2:1] == 2'b00 ? f_in_A:
-                         sw_cmd[2:1] == 2'b01 ? f_in_B:
-                         sw_cmd[2:1] == 2'b10 ? f_in_D:
-                         5'b00000;
+wire [4:0]  f_out_A     = sw_cmd[2:1] == 2'b00 ? f_in_A:
+                          sw_cmd[2:1] == 2'b01 ? f_in_B:
+                          sw_cmd[2:1] == 2'b10 ? f_in_D:
+                          5'b00000;
 
-wire [4:0]  f_out_B    = sw_cmd[0] ? 5'b00000 : f_in_B;
+wire [4:0]  f_out_B     = (sw_type == `OBF_INSN_TYPE_F || sw_type == `OBF_INSN_TYPE_FI) ?
+                          sw_cmd[4:3] == 2'b00 ? f_in_A:
+                          sw_cmd[4:3] == 2'b01 ? f_in_B:
+                          5'b00000: 
+                          sw_cmd[0] ? 5'b00000 : f_in_B;
+
 wire [15:0] f_out_I    = sw_cmd[5] ? lut_out_imm : sw_cmd[4]? 16'd0 : f_in_I;
 
 // Parse substitution command
@@ -121,16 +139,18 @@ begin
     if(obf_en) begin
         // Type-field
         case(sw_type)
-           `OBF_INSN_TYPE_A: obf_insn <= {`OR1200_OR32_ALU, f_out_D, f_out_A, f_out_B, 1'b0, f_out_OPC1, 2'b00, f_out_OPC2};
-           `OBF_INSN_TYPE_I: obf_insn <= {f_out_OPC0, f_out_D, f_out_A, f_out_I};
-           `OBF_INSN_TYPE_M: obf_insn <= {f_out_OPC0, f_out_I[15:11], f_out_A, f_out_B, f_out_I[10:0]};
-           `OBF_INSN_TYPE_N: obf_insn <= ref_insn;
-           default: obf_insn <= ref_insn; // TODO: Add some debug output
+           `OBF_INSN_TYPE_N: obf_insn = ref_insn;
+           `OBF_INSN_TYPE_A: obf_insn = {`OR1200_OR32_ALU, f_out_D, f_out_A, f_out_B, 1'b0, f_out_AOPC1, 2'b00, f_out_AOPC2};
+           `OBF_INSN_TYPE_I: obf_insn = {f_out_OPC, f_out_D, f_out_A, f_out_I};
+           `OBF_INSN_TYPE_M: obf_insn = {f_out_OPC, f_out_I[15:11], f_out_A, f_out_B, f_out_I[10:0]};
+           `OBF_INSN_TYPE_F: obf_insn = {`OR1200_OR32_SFXX, f_out_FOPC, f_out_A, f_out_B, 11'd0};
+           `OBF_INSN_TYPE_FI: obf_insn = {`OR1200_OR32_SFXXI, f_out_FOPC, f_out_A, f_out_I};
+           default: obf_insn = ref_insn; // TODO: Add some debug output
         endcase
     end 
     else begin
         // Obfuscator disabled
-        obf_insn <= ref_insn;
+        obf_insn = ref_insn;
     end
 end
 
