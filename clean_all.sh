cb6e0c388e8b5732e1bbf3a0ac2d948108a5417e
Luca Sasselli <4eversax@gmail.com>
2017-10-12 15:24:42 +0200

    Removed old scripts

--- clean_all.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf build/*
-rm -r *.log
-rm transcript
--- make_fpga.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Configuration
-TARGET_SYS=de10_lite
-
-# DO NOT MODIFY AFTER THIS LINE
-fusesoc --cores-root=fusesoc build $TARGET_SYS
