f6dd3586ef0a6fa0e058385268380fec5a14769e
Luca Sasselli <4eversax@gmail.com>
2017-10-18 10:22:36 +0200

    elf file passed as ext parameter

--- test-survivor-simple.sh
+++ test-survivor-simple.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-ELF_NAME=susan-small-corners
-ELF_PATH=/home/luca/dev/hw-obfuscator/sw/or1k-mibench/automotive/susan/susan-small-corners.elf
+ELF_FILE=$1
+ELF_DIR=$(dirname $ELF_FILE)
+ELF_FULLNAME=$(basename $ELF_FILE)
+ELF_NAME=${ELF_FULLNAME%.*}
 SIMULATOR=verilator
 SAMPLES="0 25 50 100 125 150 175 200 225 250"
 
@@ -8,7 +10,7 @@ set -e
 
 # Run reference
 echo "-> Running reference simulation..."
-./run-sim.sh -e $ELF_PATH -t ref -s $SIMULATOR -l &> /dev/null
+./run-sim.sh -e $ELF_FILE -t ref -s $SIMULATOR -l &> /dev/null
 
 # Run obfuscator
 for i in $SAMPLES; do
@@ -17,7 +19,7 @@ for i in $SAMPLES; do
     echo "--------------------------------------------------"
 
     echo "-> Running obfuscator simulation..."
-    ./run-sim.sh -e $ELF_PATH -t obf -s $SIMULATOR -f $i -l &> /dev/null
+    ./run-sim.sh -e $ELF_FILE -t obf -s $SIMULATOR -f $i -l &> /dev/null
 
     # echo "-> Checking code correctness..."
     # obf-outcheck.py build/or1200-ref-generic_0/sim-icarus/tb-executed.log build/or1200-obf-generic_0/sim-icarus/tb-executed.log
